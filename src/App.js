fc0678d4a1436b1c900559a8fffa543dc703e1df
Manishsingh1110 <dsmanishsingh@gmail.com>
2023-03-17 21:45:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import Map, {NavigationControl,Marker} from 'react-map-gl';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Map mapLib={maplibregl} 
+        initialViewState={{
+          longitude: 73.027257,
+          latitude: 19.024405,
+          zoom: 14
+        }}
+        style={{width: "100%", height: " calc(100vh)"}}
+        
+        mapStyle="https://api.maptiler.com/maps/streets-v2/style.json?key=cVWyrsbwYYISvHVDxDcq"
+      >
+        <NavigationControl position="top-left" />
+        <Marker latitude={19.024405} longitude={73.027257} color='#61dbfb'></Marker>
+        <Marker latitude={29.024405} longitude={73.027257} color='#61dbfb'></Marker>
+      </Map>
+    </div>
+  );
+}
+
+export default App;
